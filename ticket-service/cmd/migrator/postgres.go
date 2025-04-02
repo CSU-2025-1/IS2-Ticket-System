749ce9d399c9244c437a981eed49420250b453d6
YanaGnedkova <yygnedkova@mail.ru>
2025-04-02 23:20:16 +0500

    develop_ticket

---
+++ ticket-service/cmd/migrator/postgres.go
@@ -0,0 +1,30 @@
+package migrator
+
+import (
+	"auth-service/internal/repository/postgres"
+	"context"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose"
+)
+
+func MigratePostgres(ctx context.Context, config postgres.Config) error {
+	connStr := config.ToConnString()
+	db, err := goose.OpenDBWithDriver("postgres", connStr)
+	if err != nil {
+		return fmt.Errorf("migrator: %w", err)
+	}
+	defer db.Close()
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("goose dialect: %w", err)
+	}
+
+	migrationsDir := "./migrations"
+	if err := goose.Up(db, migrationsDir); err != nil {
+		return fmt.Errorf("goose up: %w", err)
+	}
+
+	fmt.Println("Migrations applied successfully!")
+	return nil
+}
